9cc91f1a2493e775cc5de0f4675f800e18dae898
arturjoaodaros-dev <arturjoaodaros@gmail.com>
2026-08-07 17:06:40 -0300

    coloquei logs completas

--- decorators/log.py
+++ decorators/log.py
@@ -2,7 +2,7 @@ def log(func):
     from datetime import datetime
     def execução(*args, **kwargs):
         resultado = func(*args, **kwargs)
-        print(datetime.now())
+        arq = open('C:\\Users\\artur\\Projects\\PythonDIO\\log.txt', 'a')
+        arq.write(f'{datetime.now()}, {func.__name__}, {args, kwargs}, {resultado} \n')
         return resultado
     return execução
-    
